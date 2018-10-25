047e869c7c1682b3a9d514410e6d5554b61f3446
Christian Barra <barrachri@gmail.com>
2018-10-25 14:20:07 +0200

    Add cfp

--- src/Components/Home/Sections/About.tsx
+++ src/Components/Home/Sections/About.tsx
@@ -13,10 +13,10 @@ export default class About extends React.Component {
                         <b>Python Pizza</b> is a micro conference organized by the Python Berlin Community.
 
                         <br />
-                        We bring the Python Community together for some amazing original Pizza™.
+                        We bring the Python Community together for some amazing original Pizza™ and Python Talks.
 
                         <br />
-                        We believe in and encourage practical sessions, in which developers share their experience and lessons from real-world projects, each talk is 10 mins lenght talk.
+                        We believe in and encourage practical sessions, in which developers share their experience and lessons from real-world projects, each talk is 10 mins long.
                     </p>
 
                 </Container>
