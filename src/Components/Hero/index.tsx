6e0aa7e139489946859ca34f9658ea229ac2bf6f
Christian Barra <barrachri@gmail.com>
2018-11-06 12:09:21 +0100

    Update location

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -11,8 +11,8 @@ export default class Hero extends React.Component {
                 <div className="hero--info">
                     <h1>Python Pizza</h1>
                     <h2>
-                        <a href="https://www.google.com/maps/place/Berlino/@52.5069296,13.1438655,10z/data=!3m1!4b1!4m5!3m4!1s0x47a84e373f035901:0x42120465b5e3b70!8m2!3d52.5200066!4d13.404954" target="_blank" rel="noopener noreferrer">
-                            Berlin @ TBA
+                        <a href="https://www.google.com/maps/place/Wooga/@52.5287069,13.4139955,17z/data=!3m1!4b1!4m5!3m4!1s0x47a84e05ac31e3a9:0x69f3e8333126bae7!8m2!3d52.5287037!4d13.4161895" target="_blank" rel="noopener noreferrer">
+                            Berlin @ Wooga
                         </a>
                     </h2>
                     <h2>23 February 2019</h2>
