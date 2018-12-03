c70d56563e670496535ee35c729ec8fcd20c7019
Christian Barra <barrachri@gmail.com>
2018-12-03 13:58:34 +0100

    Update CoC

--- src/Components/Home/Sections/Coc.tsx
+++ src/Components/Home/Sections/Coc.tsx
@@ -9,40 +9,65 @@ export default () => (
         <Container size={Sizes.medium}>
             <h1>Code of conduct</h1>
 
-            <br />
+            <h2>Purpose</h2>
+
+            <p>
+                Berlin Python Pizza is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age, religion or lack thereof.
+            </p>
+
+            <p>
+                We do not tolerate harassment of conference participants in any form.
+            </p>
+
+            <p>
+                Sexual language and imagery is not appropriate for any conference venue, including talks.
+            </p>
+
+            <p>
+                We expect all community participants to abide by this Code of Conduct in all community venues—online and in-person—as well as in all one-on-one communications pertaining to community business.
+            </p>
+
+            <p>
+                <b>Conference participants violating these rules may be sanctioned or expelled from the conference without a refund at the discretion of the conference organisers.</b>
+            </p>
 
             <h2>Expected Behavior</h2>
-            <p>
-                <ul>
-                    <li>
-                        Participate in an authentic and active way. In doing so, you contribute to the health and longevity of this community.
+            <ul>
+                <li>
+                    Participate in an authentic and active way. In doing so, you contribute to the health and longevity of this community.
                     </li>
-                    <li>
-                        Exercise consideration and respect in your speech and actions.
+                <li>
+                    Exercise consideration and respect in your speech and actions.
                     </li>
-                    <li>
-                        Attempt collaboration before conflict.
+                <li>
+                    Attempt collaboration before conflict
                     </li>
-                    <li>
-                        Refrain from demeaning, discriminatory, or harassing behavior and speech.
+                <li>
+                    Use inclusive language
                     </li>
-                    <li>
-                        Be mindful of your surroundings and of your fellow participants. Alert community leaders if you notice a dangerous situation, someone in distress, or violations of this Code of Conduct, even if they seem inconsequential.
+                <li>
+                    Refrain from demeaning, discriminatory, or harassing behavior and speech.
                     </li>
-                </ul>
-            </p>
-
-            <br />
+                <li>
+                    Be mindful of your surroundings and of your fellow participants.
+                    </li>
+                <li>
+                    Alert community leaders if you notice a dangerous situation, someone in distress, or violations of this Code of Conduct, even if they seem inconsequential.
+                    </li>
+            </ul>
 
             <h2>Unacceptable Behavior</h2>
-            <p>
-                Unacceptable behaviors include: intimidating, harassing, abusive, discriminatory, derogatory or demeaning speech or actions by any participant in our community online, at all related events and in one-on-one communications carried out in the context of community business. Community event venues may be shared with members of the public; please be respectful to all patrons of these locations.
-            </p>
-            <p>
-                Harassment includes: harmful or prejudicial verbal or written comments related to gender, sexual orientation, race, religion, disability; inappropriate use of nudity and/or sexual images (including presentation slides); inappropriate depictions of violence (including presentation slides); deliberate intimidation, stalking or following; harassing photography or recording; sustained disruption of talks or other events; inappropriate physical contact, and unwelcome sexual attention.
-            </p>
-
-            <br />
+            <p>Unacceptable behaviors include, but is not limited to:</p>
+            <ul className="ul-bad">
+                <li>Abusive, discriminatory, derogatory or demeaning speech or actions</li>
+                <li>Sexual images in public spaces</li>
+                <li>Harassing, intimidation, stalking, or following</li>
+                <li>Sustained disruption of talks or other events</li>
+                <li>Inappropriate physical contact</li>
+                <li>Invasion of personal space</li>
+                <li>Unwelcome sexual attention</li>
+                <li>Advocating for, or encouraging, any of the above behaviour</li>
+            </ul>
 
             <h2>Consequences of Unacceptable Behavior</h2>
             <p>
@@ -61,14 +86,52 @@ export default () => (
 
             <h2>If You Witness or Are Subject to Unacceptable Behavior</h2>
             <p>
-                If you are subject to or witness unacceptable behavior, or have any other concerns, please notify a community organizer as soon as possible. You can find a list of organizers to contact for each of the supporters of this code of conduct at the bottom of this page.
+                If you are subject to or witness unacceptable behavior, or have any other concerns, please notify a community organizer as soon as possible.
             </p>
 
             <p>
-                Additionally, community organizers are available to help community members engage with local law enforcement or to otherwise help those experiencing unacceptable behavior feel safe. In the context of in-person events, organizers will also provide escorts as desired by the person experiencing distress.
+                You can make a personal report by:
+                <br />
+                <br />
+                - Contacting Christian Barra <br />
+                - Email: barrachri+berlinpythonpizza@gmail.com <br />
+                - Phone: We’ll publish a phone number that is staffed during main conference hours.
             </p>
 
-            <br />
+            <p>
+                When taking a personal report, our staff will ensure you are safe and cannot be overheard.
+                They may involve other event staff to ensure your report is managed properly.
+            </p>
+
+            <p>
+                Once safe, we’ll ask you to tell us about what happened.
+                This can be upsetting, but we’ll handle it as respectfully as possible, and you can bring someone to support you.
+                You won’t be asked to confront anyone and we won’t tell anyone who you are.
+            </p>
+
+            <p>
+                Our team will be happy to help you contact hotel/venue security, local law enforcement, local support services, provide escorts, or otherwise assist you to feel safe for the duration of the event.
+            </p>
+
+            <p>
+                We value your attendance.
+            </p>
+
+            <p>
+                Important contact numbers:
+                <br />
+                <br />
+                Police: Keibelstraße 35, 10178 Berlin, phone number 110<br />
+                Medical Emergency: Call 112.
+            </p>
+
+            <h2>Anonymous report</h2>
+            <p>
+                You can make an anonymous report <a href="https://goo.gl/forms/IUXFQVNtET7eg4Or1">here</a>.
+            </p>
+            <p>
+                We can’t follow up an anonymous report with you directly, but we will fully investigate it and take whatever action is necessary to prevent a recurrence.
+            </p>
 
             <h2>Addressing Grievances</h2>
             <p>
@@ -79,22 +142,22 @@ export default () => (
                 Your grievance will be handled in accordance with our existing governing policies.
             </p>
 
-            <br />
-
-            <h2>Scope</h2>
+            <h2>Credits</h2>
             <p>
-                We expect all community participants (contributors, paid or otherwise; sponsors; and other guests) to abide by this Code of Conduct in all community venues—online and in-person—as well as in all one-on-one communications pertaining to community business.
-            </p>
-
-            <p>
-                Inspired by the{' '}
+                This Code of Conduct was inspired by the{' '}
                 <a
                     rel="noopener noreferrer"
                     target="_blank"
                     href="https://berlincodeofconduct.org/"
                 >
-                    Berlin Code of Conduct
-                </a>.
+                    Berlin
+                </a> and{' '} <a
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href="https://2018.jsconf.eu/code-of-conduct/"
+                >
+                    JS Conf
+                </a> code of conducts.
             </p>
         </Container>
     </section >
